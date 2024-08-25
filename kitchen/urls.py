687286847d8c9f7e197d85094b5a89222dd02fae
turb0 <nazar.mc28@gmail.com>
2024-08-25 16:47:31 +0200

    create basic templates for pages

---
+++ kitchen/urls.py
@@ -0,0 +1,13 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('dishes/', views.dishes, name='dishes'),
+    path('cooks/', views.cooks, name='cooks'),
+    path('ingridients/', views.ingridients, name='ingridients'),
+]
+
--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Hello, world! This is the home page.")
+
+def ingridients(request):
+    return HttpResponse("Hello, world! This is the ingridients page.")
+
+def register(request):
+    return HttpResponse("Hello, world! This is the register page.")
+
+def login(request):
+    return HttpResponse("Hello, world! This is the login page.")
+
+def dishes(request):
+    return HttpResponse("Hello, world! This is the dishes page.")
+
+
+def cooks(request):
+    return HttpResponse("Hello, world! This is the cooks page.")
+
