c5aea36beed12c795a595a51947e963da3c438ee
turb0 <nazar.mc28@gmail.com>
2024-10-10 10:46:28 +0200

    rename to kitchenservice

--- kitchenservice/urls.py
+++ kitchenservice/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for kitchenservece1 project.
+URL configuration for kitchenservice project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
--- kitchenservice/wsgi.py
+++ kitchenservice/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for kitchenservece1 project.
+WSGI config for kitchenservice project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kitchenservece1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kitchenservice.settings')
 
 application = get_wsgi_application()
