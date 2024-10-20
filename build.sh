f7b5b654e66779c156402385287c40b3a642a6da
turb0 <nazar.mc28@gmail.com>
2024-10-20 22:40:07 +0200

    build.sh fix

--- build.sh
+++ build.sh
@@ -2,16 +2,14 @@
 # Exit on error
 set -o errexit
 
-
-# Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r requirements.txt
-
+# Install dependencies using Poetry
+poetry install
 
 # Convert static asset files
-python manage.py collectstatic --no-input
-
+poetry run python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python manage.py migrate
+poetry run python manage.py migrate
 
-gunicorn kitchenservice.wsgi:application --bind 0.0.0.0:$PORT
+# Start Gunicorn server
+poetry run gunicorn kitchenservice.wsgi:application --bind 0.0.0.0:$PORT
