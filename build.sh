0c136228f9d78f726fb68829b8395ade0e8bbf83
turb0 <nazar.mc28@gmail.com>
2024-10-20 22:46:14 +0200

    build.sh test

--- build.sh
+++ build.sh
@@ -2,14 +2,20 @@
 # Exit on error
 set -o errexit
 
+# Check if Python 3 is installed
+if ! command -v python3 &> /dev/null; then
+    echo "Python 3 could not be found. Please ensure Python is installed."
+    exit 1
+fi
+
 # Install dependencies using Poetry
 poetry install
 
 # Convert static asset files
-poetry run python manage.py collectstatic --no-input
+poetry run python3 manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-poetry run python manage.py migrate
+poetry run python3 manage.py migrate
 
 # Start Gunicorn server
 poetry run gunicorn kitchenservice.wsgi:application --bind 0.0.0.0:$PORT
