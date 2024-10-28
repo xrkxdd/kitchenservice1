f02cbca919f353a3b3f95295795a7eca87edee20
turb0 <nazar.mc28@gmail.com>
2024-10-28 18:22:55 +0100

    base_dir static change

--- kitchenservice/settings.py
+++ kitchenservice/settings.py
@@ -132,7 +132,7 @@ STATIC_ROOT = "staticfiles"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "C:/projects/kitchenservece1/static",
 ]
 
 
