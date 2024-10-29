2cf241476c308110ed3ce2446074787b007bfdc7
turb0 <nazar.mc28@gmail.com>
2024-10-29 18:26:46 +0100

    host fix4

--- kitchenservice/settings.py
+++ kitchenservice/settings.py
@@ -62,8 +62,7 @@ ROOT_URLCONF = 'kitchenservice.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,6 +74,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'kitchenservice.wsgi.application'
 
