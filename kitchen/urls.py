c80be4c28c8c951a8ad57ff6db33221e4e595f8c
turb0 <nazar.mc28@gmail.com>
2024-11-08 08:15:13 +0100

    testtt

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,35 +1,39 @@
 from django.urls import path
+from django.contrib import admin
 from django.contrib.auth.views import LogoutView
+from django.urls import include
 from .views import (
-    HomeView, CustomLoginView, IngredientListView, IngredientCreateView, RecipeCreateView,
+    HomeView, IngredientListView, IngredientCreateView, RecipeCreateView,
     RecipeListView, ChefListView, ChefCreateView, DishTypeListView, DishTypeCreateView,
-    ChefDeleteView, IngredientDeleteView, RecipeDeleteView, DishTypeDeleteView
+    ChefDeleteView, IngredientDeleteView, RecipeDeleteView, DishTypeDeleteView,
+    CustomLoginView
 )
 
 app_name = 'kitchen'
 
 urlpatterns = [
-    path('', CustomLoginView.as_view(), name='login'),  # Login page (home page as login page)
-    path('home/', HomeView.as_view(), name='home'),  # Home page
-    path('logout/', LogoutView.as_view(), name='logout'),  # URL for logging out
+    path('', CustomLoginView.as_view(), name='login'),  # Главная страница логина
+    path('home/', HomeView.as_view(), name='home'),  # Главная страница после логина
+    path('admin/', admin.site.urls),  # Админка
 
-    # Dishes (recipes) related URLs
-    path('home/dishes/', RecipeListView.as_view(), name='dishes'),  # View list of dishes
-    path('dishes/create/', RecipeCreateView.as_view(), name='dishes_create'),  # Create a new dish
+    path('accounts/', include('django.contrib.auth.urls')),  # Все стандартные урлы для аутентификации
+
+
+    path('home/dishes/', RecipeListView.as_view(), name='dishes'),
+    path('dishes/create/', RecipeCreateView.as_view(), name='dishes_create'),
     path('home/dishes/delete/<int:pk>/', RecipeDeleteView.as_view(), name='recipe_delete'),
 
-    # Cooks (chefs) related URLs
-    path('home/cooks/', ChefListView.as_view(), name='cooks'),  # View list of cooks
-    path('home/cooks_create/', ChefCreateView.as_view(), name='cooks_create'),  # Create a new cook
-    path('home/cooks/delete/<int:pk>/', ChefDeleteView.as_view(), name='chef_delete'),  # Delete a specific cook by ID
+    path('home/cooks/', ChefListView.as_view(), name='cooks'),
+    path('home/cooks_create/', ChefCreateView.as_view(), name='cooks_create'),
+    path('home/cooks/delete/<int:pk>/', ChefDeleteView.as_view(), name='chef_delete'),
 
-    # Ingredients related URLs
-    path('home/ingredients/', IngredientListView.as_view(), name='ingredients'),  # View list of ingredients
-    path('home/ingredients_create/', IngredientCreateView.as_view(), name='ingredient_create'),  # Create a new ingredient
-    path('home/ingredients/delete/<int:pk>/', IngredientDeleteView.as_view(), name='ingredient_delete'),  # Delete a specific ingredient by ID
 
-    # Dish types related URLs
-    path('home/dishtype/', DishTypeListView.as_view(), name='dishtype'),  # View list of dish types
-    path('home/dishtype_create/', DishTypeCreateView.as_view(), name='dishtype_create'),  # Create a new dish type
-    path('home/dishtype/delete/<int:pk>/', DishTypeDeleteView.as_view(), name='dishtype_delete'),  # Delete a specific dish type by ID
+    path('home/ingredients/', IngredientListView.as_view(), name='ingredients'),
+    path('home/ingredients_create/', IngredientCreateView.as_view(), name='ingredient_create'),
+    path('home/ingredients/delete/<int:pk>/', IngredientDeleteView.as_view(), name='ingredient_delete'),
+
+
+    path('home/dishtype/', DishTypeListView.as_view(), name='dishtype'),
+    path('home/dishtype_create/', DishTypeCreateView.as_view(), name='dishtype_create'),
+    path('home/dishtype/delete/<int:pk>/', DishTypeDeleteView.as_view(), name='dishtype_delete'),
 ]
--- kitchen/views.py
+++ kitchen/views.py
@@ -24,12 +24,7 @@ class HomeView(TemplateView):
 # Login page
 class CustomLoginView(LoginView):
     template_name = 'registration/login.html'
-    redirect_authenticated_user = True  # Перенаправлення залогінених користувачів
-    next_page = reverse_lazy('kitchen:home')
-
-    def form_invalid(self, form):
-        messages.error(self.request, "Invalid username or password.")
-        return super().form_invalid(form)
+    redirect_authenticated_user = True
 
 # View and add ingredients
 class IngredientListView(CreateView, ListView):
