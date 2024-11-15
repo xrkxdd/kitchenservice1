841f29788f5d973e21410ed13854098981e95e27
turb0 <nazar.mc28@gmail.com>
2024-11-15 18:52:12 +0100

    testcase2

--- kitchenservice/settings.py
+++ kitchenservice/settings.py
@@ -41,7 +41,7 @@ ROOT_URLCONF = 'kitchenservice.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
