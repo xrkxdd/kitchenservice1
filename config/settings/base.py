d4ef5472fd1add9c219b1ad1b0d22e88de9f98db
turb0 <nazar.mc28@gmail.com>
2024-11-15 19:06:06 +0100

    testw

--- config/settings/base.py
+++ config/settings/base.py
@@ -110,6 +110,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = "staticfiles"
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
